129d8c9c8b9942a043945e97c5beee6d48572185
Manish <maaneskarki111@gmail.com>
2022-11-07 14:06:36 +1100

    initial serve

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div>
+
+<footer class="footer bg-dark text-light text-center py-5 mt-5">
+        Copyritght &copy; all reserved. Made with passion by me.
+      </footer>
+
+      <a href="#wrapper" class="goup" id="goup">
+        <i class="fa-solid fa-circle-up fa-3x text-primary"></i>
+      </a>
+    </div>
+  )
+}
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import my from "../assets/my.png";
+// import my from "../assets/my.jpeg";
 import { TopNav } from "./TopNav";
 import { Container, Row, Col, Button } from "react-bootstrap";
 
@@ -11,7 +11,7 @@ export const Hero = () => {
       <Container>
         <Row className="mt-5 py-3">
           <Col md="6" className="text-center order-md-2">
-            {/* <img src={my} width="80%" alt="" /> */}
+            {/* <img src={my} width="80%" alt="" /> */ }
           </Col>
           <Col md="6" className="mt-3 pb-3">
             Hi 👋 i'm
